80768acd284e6d8e77463f9936e4194b7f600eec
James Cheese <jamesc@intouch-ltd.com>
2019-04-25 15:09:25 +0100

    0.3.1, bumping version number, adding README to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
+# Load README
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
 	name='intouch_queryset_csv',
-	version='0.3.0',
+	version='0.3.1',
 	description='Module to export django querysets as csv files',
 	maintainer='James Cheese',
 	maintainer_email='james.cheese@intouch-ltd.com',
 	url='https://github.com/intouch-smartwater/queryset-csv',
 	packages=['intouch', 'intouch.queryset_csv'],
 	keywords='django queryset csv',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
 	classifiers = [
 		'Development Status :: 3 - Alpha',
 		'Environment :: Plugins',
